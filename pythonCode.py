0017fd0281742e70c07cc9bf1b758c91e5ba1ed9
lizbetancourt <lizbetancourt007@gmail.com>
2021-03-01 10:13:00 -0500

    Update pythonCode.py
    ajuste

--- pythonCode.py
+++ pythonCode.py
@@ -1,7 +1,15 @@
 
+from aplicacion.db import db
 
+class Paciente(db.Model)
+  _tablename_: 'paciente'
 
-def prueba(datos):
-  print("entroooo")
+  id= db.column(db.Integer, primary_key=True)
+  nombre= db.column(db.string(200), nullable=False)
+  numeroHistoria= db.column(db.Integer, nullable=False)
+  fuma=db.column(db.Integer, nullable=False)
+  tieneDieta=db.column(db.Integer, nullable=False)
+  peso=db.column(db.Integer, nullable=False)
+  estatura=db.column(db.Integer, nullable=False)
 
 
